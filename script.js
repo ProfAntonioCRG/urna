ccbc029844c0cffc07f5ad187e01ace7aeccc005
ProfAntonioCRG <163427510+ProfAntonioCRG@users.noreply.github.com>
2025-02-19 22:10:50 -0300

    03

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let numeroDigitado = "";
+
+function teclar(num) {
+    let somTecla = document.getElementById("tecla-som");
+    somTecla.play();
+    
+    if (numeroDigitado.length < 2) {
+        numeroDigitado += num;
+        document.getElementById("numero").innerText = numeroDigitado;
+    }
+}
+
+function limpar() {
+    numeroDigitado = "";
+    document.getElementById("numero").innerText = "";
+    document.getElementById("mensagem").innerText = "";
+}
+
+function confirmar() {
+    let somFim = document.getElementById("fim-som");
+    somFim.play();
+    
+    if (numeroDigitado.length === 2) {
+        document.getElementById("mensagem").innerText = "Voto confirmado!";
+    } else {
+        document.getElementById("mensagem").innerText = "Número inválido!";
+    }
+}
 
